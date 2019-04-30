ee3940e37140e08a577e2d3074057dde9ad8c82d
Nicholas Orochena <nicholas.orochena@gmail.com>
2019-04-30 04:47:12 +0000

    Automatically backed up by Learn

--- prime.rb
+++ prime.rb
@@ -1,7 +1,14 @@
 # Add  code here!
 def prime?(int)
   arr = (2...int).to_a
-  puts arr
+  
+  arr.each do |ele|
+    if (int % ele == 0)
+      false
+    else
+      true
+    end
+  end
 end
 
 prime?(10)
